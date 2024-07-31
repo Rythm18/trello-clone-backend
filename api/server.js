0bacf88a1275657fddfed6924b54d40805401c30
Ridham <ridhamk.j@gmail.com>
2024-07-31 14:49:47 +0530

    vercel deployment

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const rootRouter = require("./routes/index");
+const rootRouter = require("../routes/index");
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose.connect(process.env.DATABASE_URL).then(console.log("connexr")).catch((error) => console.log(error));
@@ -7,9 +7,13 @@ mongoose.connect(process.env.DATABASE_URL).then(console.log("connexr")).catch((e
 const cors = require("cors");
 
 const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
 app.listen(5000);
+
+module.exports = app;
