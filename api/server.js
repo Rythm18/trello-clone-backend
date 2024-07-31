a6ebda94fb070265f29f79b7b0d5b157cdaa1843
Ridham <ridhamk.j@gmail.com>
2024-07-31 16:27:17 +0530

    cors resolve

--- api/server.js
+++ api/server.js
@@ -8,12 +8,22 @@ const cors = require("cors");
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   };
   
-app.use(cors(corsOptions));
+  app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use(cors());
